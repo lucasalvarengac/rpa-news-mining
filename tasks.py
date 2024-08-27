0e798d09510c5905a7932b232992475ecf3fc87e
Lucas Alvarenga <lucas.alvarenga92@gmail.com>
2024-08-27 16:24:25 -0300

    fix for chrome driver

--- tasks.py
+++ tasks.py
@@ -61,11 +61,13 @@ class Crawler:
 
     def set_chrome_options(self):
         options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
         options.add_argument('--disable-web-security')
         options.add_argument("--start-maximized")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         return options
 
